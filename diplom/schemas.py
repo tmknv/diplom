537913f696990081e22452644bd4e1180c3eed08
tmknv <tmknv@MacBook-Pro-tmknv.local>
2026-03-31 17:44:50 +0300

    структура данных Metrics

--- diplom/schemas.py
+++ diplom/schemas.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
 
+from dataclasses import dataclass, field
+
+# @dataclass
 class Metrics(BaseModel):
     validator_name: str
     model_name: str
     f1_score: float = 0
     meta: dict = {}
+    # meta: dict = field(default_factory=dict)
 
