3f1428975a6449189e561e10ab9bb4aa82be8130
tmknv <tmknv@MacBook-Pro-tmknv.local>
2026-06-02 20:14:24 +0300

    добавил создание датасета для дистилляции ризонинга

--- diplom/data_processing/data_preprocessing.py
+++ diplom/data_processing/data_preprocessing.py
@@ -1,3 +1,4 @@
+# diplom/data_processing/data_preprocessing.py
 """
 Модуль для:
     1. Генерации ризонинга моделькой учителем
@@ -5,6 +6,7 @@
     3. Итоговый датасет будет указанной размерности
 """
 
+import re 
 
 import requests
 
@@ -44,6 +46,54 @@ class DataProcessor:
     def __init__(self, dataset):
         self.dataset = dataset
     
+
+    def _extract_reasoning(self, sample):
+        """
+        Извлекает reasoning из answer и удаляет GSM8K markup:
+        <<8*3=24>>
+
+        Args:
+            sample (dict): Элемент датасета.
+
+        Returns:
+            dict: reasoning для элемента датасета.
+        """
+        try:
+            # Всё до ####
+            cleaned = sample["answer"].split("####")[0].strip()
+
+            # Удаляем <<...>>
+            cleaned = re.sub(r"<<.*?>>", "", cleaned)
+
+            # Нормализуем запятые
+            cleaned = cleaned.replace(",", ".")
+
+            # Убираем лишние пробелы
+            cleaned = re.sub(r"\s+", " ", cleaned).strip()
+
+        except Exception:
+            cleaned = None
+
+        return {"reasoning": cleaned}
+    
+
+    def _extract_answer(self, example):
+        """
+        Извлекает финальный ответ из поля `answer`.
+
+        Args:
+            example (dict): Пример из датасета.
+
+        Returns:
+            dict: Обновлённый пример с новым полем.
+        """
+        try:
+            cleaned = example["answer"].split("####")[1].strip().replace(",",'.')
+        except Exception:
+            cleaned = None
+
+        return {"one_num_answer": cleaned}
+    
     def get_processed_dataset(self):
         """
         Приводит датасет к виду, удобному для подсчёта F1-score.
@@ -71,34 +121,49 @@ class DataProcessor:
         if dataset is None:
             logger.error("dataset is None")
             return None
-
-        def _extract_answer(example):
-            """
-            Извлекает финальный ответ из поля `answer`.
-
-            Args:
-                example (dict): Пример из датасета.
-
-            Returns:
-                dict: Обновлённый пример с новым полем.
-            """
-            try:
-                cleaned = example["answer"].split("####")[1].strip().replace(",",'.')
-            except Exception:
-                cleaned = None
-
-            return {"one_num_answer": cleaned}
-
+        
         # применяем ко всем сплитам (train/test)
-        dataset = dataset.map(_extract_answer)
+        dataset = dataset.map(self._extract_answer)
+
+        return dataset
+    
+    def get_df_reasoning_destil(self):
+        """
+        Приводит датасет к виду, необходимому для дистилляции ризонинга.
+
+        Для датасета openai/gsm8k добавляется новые колонки:
+        1.  `cleaned_answer_for_f1`. Колнака, содержащая только итоговый числовой ответ.
+        2. `reasoning`. Колонка, содержащая рассуждения.
+
+        Returns:
+            datasets.DatasetDict | None:
+                Обработанный датасет с новыми колонками
+        """
+
+        dataset = self.dataset
+
+        if dataset is None:
+            logger.error("dataset is None")
+            return None
+        
+        # применяем ко всем сплитам (train/test)
+        dataset = dataset.map(self._extract_answer)
+        dataset = dataset.map(self._extract_reasoning)
 
         return dataset
 
+        
 
-class DataProcessorTraining:
-    """
-    Класс для:
-        1. Получения данных для разного обучения
-        2. Сохранения обработанных данных
-    """
+
+
+
+
+
+# class DataProcessorTraining:
+#     """
+#     Класс для:
+#         1. Получения данных для разного обучения
+#         2. Сохранения обработанных данных
+#     """
+
 
